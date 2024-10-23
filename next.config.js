c21e2eea9d196d1c5c07c84c7b66295ba47c0b6d
thulani <thulanim457@gmail.com>
2024-10-23 19:15:10 +0200

    .

--- next.config.js
+++ next.config.js
@@ -34,13 +34,49 @@ const nextConfig = {
 			},
 		];
 	},
-	webpack: (config, { isServer }) => {
-		if (!isServer) {
-			config.resolve.fallback = {
-				fs: false,
-				net: false,
-				tls: false,
-				crypto: false,
+	webpack: (config, { isServer, dev }) => {
+		if (!isServer && !dev) {
+			config.optimization.splitChunks = {
+				chunks: 'all',
+				minSize: 20000,
+				maxSize: 244000,
+				minChunks: 1,
+				maxAsyncRequests: 30,
+				maxInitialRequests: 30,
+				enforceSizeThreshold: 50000,
+				cacheGroups: {
+					defaultVendors: false,
+					default: false,
+					framework: {
+						chunks: 'all',
+						name: 'framework',
+						test: /(?<!node_modules.*)[\\/]node_modules[\\/](react|react-dom|scheduler|prop-types|use-subscription)[\\/]/,
+						priority: 40,
+						enforce: true,
+					},
+					commons: {
+						name: 'commons',
+						chunks: 'initial',
+						minChunks: 2,
+						priority: 20,
+					},
+					lib: {
+						test(module) {
+							return (
+								module.size() > 160000 &&
+								/node_modules[/\\]/.test(module.identifier())
+							);
+						},
+						name(module) {
+							const hash = crypto.createHash('sha1');
+							hash.update(module.identifier());
+							return hash.digest('hex').substring(0, 8);
+						},
+						priority: 30,
+						minChunks: 1,
+						reuseExistingChunk: true,
+					},
+				},
 			};
 		}
 		return config;
