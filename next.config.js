5ed33585158368553faacd8d4e5c873879daadd4
thulani <thulanim457@gmail.com>
2024-10-23 19:08:44 +0200

    .

--- next.config.js
+++ next.config.js
@@ -21,18 +21,34 @@ const nextConfig = {
 		],
 		unoptimized: true,
 	},
-	async rewrites() {
+	async headers() {
 		return [
 			{
-				source: '/:path*',
-				destination: '/:path*',
+				source: '/site.webmanifest',
+				headers: [
+					{
+						key: 'Content-Type',
+						value: 'application/manifest+json',
+					},
+				],
 			},
 		];
 	},
+	webpack: (config, { isServer }) => {
+		if (!isServer) {
+			config.resolve.fallback = {
+				fs: false,
+				net: false,
+				tls: false,
+				crypto: false,
+			};
+		}
+		return config;
+	},
+	poweredByHeader: false,
+	generateEtags: false,
 	trailingSlash: false,
 	output: 'standalone',
-	assetPrefix: '',
-	basePath: '',
 };
 
 module.exports = nextConfig;
