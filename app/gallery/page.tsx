8deeefa28a8a8cc44d13f7840c1bbb03463db29c
thulani <thulanim457@gmail.com>
2024-10-25 09:15:10 +0200

    UI updates

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -52,8 +52,9 @@ export default function Gallery() {
   }, [selectedImage])
 
   return (
-    <div className="relative min-h-screen bg-black text-white">
-      <section className="py-8 sm:py-12">
+    <div className="relative min-h-screen bg-black text-white bg-[url('/gallery-image.jpg')] bg-cover bg-center bg-fixed">
+      <div className="absolute inset-0 bg-black bg-opacity-70"></div>
+      <section className="relative py-8 sm:py-12">
         <div className="container mx-auto px-4">
           <motion.div
             className="text-center max-w-3xl mx-auto mb-12"
